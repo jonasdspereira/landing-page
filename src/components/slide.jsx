9a9420e018fb2bd9e5577aab90a83094679d0f15
Jonas <66091286+jonasdspereira@users.noreply.github.com>
2022-07-22 14:53:07 -0400

    2.0

---
+++ src/components/slide.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const slideImages = [
+  {
+    url: 'images/slide_2.jpg',
+    caption: 'Slide 1'
+  },
+  {
+    url: 'images/slide_3.jpg',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'images/slide_4.jpg',
+    caption: 'Slide 3'
+  },
+];
+
+const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    )
+}
